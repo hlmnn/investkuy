2f72556544df24bc3d677fc53a77ebe7e41420fb
Hilman Fauzi Herdiana <kk726627@gmail.com>
2023-05-21 10:09:47 +0700

    add login_choice_screen, login_screen, register_screen

---
+++ lib/ui/screen/login/login_screen.dart
@@ -0,0 +1,155 @@
+import 'package:flutter/material.dart';
+import 'package:investkuy/ui/screen/investor/topup_screen.dart';
+import 'package:investkuy/ui/screen/investor/withdraw_screen.dart';
+import 'package:investkuy/ui/screen/register/register_screen.dart';
+
+class Login extends StatefulWidget {
+  const Login({super.key, required this.title});
+
+  final String title;
+
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  bool _isObsecure = true;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.white,
+        body: Center(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(20),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    // Image
+                    Padding(
+                        padding: const EdgeInsets.only(top: 10, bottom: 10),
+                        child: Image.asset('assets/images/logo.png')
+                    ),
+
+                    Form(
+                      child: Column(
+                        children: [
+                          // Username
+                          Padding(
+                            padding: const EdgeInsets.only(bottom: 5),
+                            child: TextFormField(
+                              decoration: const InputDecoration(
+                                border: OutlineInputBorder(
+                                  borderSide: BorderSide(color: Color(0xff146C94)),
+                                ),
+                                errorBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(color: Colors.red),
+                                ),
+                                contentPadding: EdgeInsets.all(10),
+                                hintText: 'Username',
+                                hintStyle: TextStyle(
+                                  color: Color(0xff4A4A4A),
+                                  fontSize: 14,
+                                ),
+                              ),
+                            ),
+                          ),
+
+                          // Password
+                          Padding(
+                            padding: const EdgeInsets.only(top: 5),
+                            child: TextFormField(
+                              obscureText: _isObsecure,
+                              decoration: InputDecoration(
+                                  border: const OutlineInputBorder(
+                                    borderSide: BorderSide(color: Color(0xff146C94)),
+                                  ),
+                                  errorBorder: const OutlineInputBorder(
+                                    borderSide: BorderSide(color: Colors.red),
+                                  ),
+                                  contentPadding: const EdgeInsets.all(10),
+                                  hintText: 'Password',
+                                  hintStyle: const TextStyle(
+                                    color: Color(0xff4A4A4A),
+                                    fontSize: 14,
+                                  ),
+                                  suffixIcon: IconButton(
+                                    icon: Icon(_isObsecure ? Icons.visibility_off : Icons.visibility),
+                                    onPressed: () {
+                                      setState(() {
+                                        _isObsecure = !_isObsecure;
+                                      });
+                                    },
+                                  ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+
+                    // Button
+                    Padding(
+                      padding: const EdgeInsets.only(top: 10),
+                      child: ElevatedButton(
+                        onPressed:  () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => const Withdraw(title: 'Withdraw')),
+                          );
+
+                          // if (widget.title == 'investor') {
+                          //
+                          // } else {
+                          //
+                          // }
+                        },
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: const Color(0xff19A7CE),
+                            fixedSize: const Size(220, 50),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10)
+                            )
+                        ),
+                        child: const Text(
+                          "Masuk",
+                          style: TextStyle(
+                            fontSize: 18,
+                          ),
+                        ),
+                      ),
+                    ),
+
+                    // Text and TextButton
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text("Belum punya akun?"),
+                        TextButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => const Register(title: 'Register')),
+                            );
+                          },
+                          style: TextButton.styleFrom(
+                              backgroundColor: Colors.transparent
+                          ),
+                          child: const Text(
+                            "Daftar disini!",
+                            style: TextStyle(
+                              color: Colors.black,
+                              decoration: TextDecoration.underline,
+                            ),
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            )
+        )
+    );
+  }
+}
---
+++ lib/ui/screen/visitor/login_choice_screen.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:investkuy/ui/screen/login/login_screen.dart';
+
+class LoginChoice extends StatefulWidget {
+  const LoginChoice({super.key, required this.title});
+
+  final String title;
+
+  @override
+  _LoginChoiceState createState() => _LoginChoiceState();
+}
+
+class _LoginChoiceState extends State<LoginChoice> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // Image
+              Padding(
+                  padding: const EdgeInsets.only(top: 10, bottom: 10),
+                  child: Image.asset('assets/images/logo.png')
+              ),
+
+              // Login as Investor
+              Padding(
+                padding: const EdgeInsets.only(top: 10, bottom: 5),
+                child: ElevatedButton(
+                  onPressed:  () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const Login(title: 'investor')),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xff19A7CE),
+                      fixedSize: const Size(double.maxFinite, 50),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)
+                      )
+                  ),
+                  child: const Text(
+                    "Masuk Sebagai Investor",
+                    style: TextStyle(
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
+              ),
+
+              // Login as UMKM
+              Padding(
+                padding: const EdgeInsets.only(top: 5, bottom: 10),
+                child: ElevatedButton(
+                  onPressed:  () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const Login(title: 'umkm')),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xff19A7CE),
+                      fixedSize: const Size(double.maxFinite, 50),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)
+                      )
+                  ),
+                  child: const Text(
+                    "Masuk Sebagai UMKM",
+                    style: TextStyle(
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
