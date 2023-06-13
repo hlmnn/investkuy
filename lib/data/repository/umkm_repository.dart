1ab51077f441bb0147a4756ca4919e3a984bbc8e
Ghifari Octaverin <ghifariocta21@gmail.com>
2023-06-13 20:18:38 +0700

    add umkm repository and model

---
+++ lib/data/repository/umkm_repository.dart
@@ -0,0 +1,19 @@
+import 'package:dio/dio.dart';
+
+class UmkmRepository {
+  late final _dio;
+
+  // Constructor
+  UmkmRepository() {
+    _dio = Dio(
+      BaseOptions(
+        baseUrl: "http://18.140.79.85",
+        connectTimeout: const Duration(seconds: 5),
+        receiveTimeout: const Duration(seconds: 5),
+      ),
+    );
+    // Log Interceptor
+    _dio.interceptors
+        .add(LogInterceptor(requestBody: true, responseBody: true));
+  }
+}
