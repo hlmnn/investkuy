6c34b1c59e6d8a0a94f4e5406e0aede3e195f340
Ghifari Octaverin <ghifariocta21@gmail.com>
2023-06-15 21:14:48 +0700

    setup riwayat repository

---
+++ lib/data/repository/riwayat_repository.dart
@@ -0,0 +1,19 @@
+import 'package:dio/dio.dart';
+
+class RiwayatRepository {
+  late final Dio _dio;
+
+  // Constructor
+  RiwayatRepository() {
+    _dio = Dio(
+      BaseOptions(
+        baseUrl: "http://18.140.79.85",
+        connectTimeout: const Duration(seconds: 5),
+        receiveTimeout: const Duration(seconds: 5),
+      ),
+    );
+    // Log Interceptor
+    _dio.interceptors
+        .add(LogInterceptor(requestBody: true, responseBody: true));
+  }
+}
