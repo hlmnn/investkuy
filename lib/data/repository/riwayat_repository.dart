403575ffddba0fb88c17db870518cb6f556cae72
Ghifari Octaverin <ghifariocta21@gmail.com>
2023-06-16 15:46:03 +0700

    add riwayat pendanaan feature - not finishhed yet

--- lib/data/repository/riwayat_repository.dart
+++ lib/data/repository/riwayat_repository.dart
@@ -1,4 +1,7 @@
+import 'dart:developer';
 import 'package:dio/dio.dart';
+import 'package:investkuy/data/model/pendanaan_model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class RiwayatRepository {
   late final Dio _dio;
@@ -15,5 +18,47 @@ class RiwayatRepository {
     // Log Interceptor
     _dio.interceptors
         .add(LogInterceptor(requestBody: true, responseBody: true));
+  }
+
+  Future<List<InProgressPendanaanModel>> getInProgressPendanaan() async {
+    try {
+      final id = await getId();
+      final response = await _dio.get("/pendanaan/riwayat-in-progress/$id");
+      List<InProgressPendanaanModel> data = [];
+      for (var val in response.data['data']) {
+        data.add(InProgressPendanaanModel.fromJson(val));
+      }
+      return data;
+    } on DioException catch (e) {
+      log(e.response!.statusCode.toString());
+      log(e.message.toString());
+      rethrow;
+    }
+  }
+
+  Future<List<CompletedPendanaanModel>> getCompletedPendanaan() async {
+    try {
+      final id = await getId();
+      final response = await _dio.get("/pendanaan/riwayat-completed/$id");
+      List<CompletedPendanaanModel> data = [];
+      for (var val in response.data['data']) {
+        data.add(CompletedPendanaanModel.fromJson(val));
+      }
+      return data;
+    } on DioException catch (e) {
+      log(e.response!.statusCode.toString());
+      log(e.message.toString());
+      rethrow;
+    }
+  }
+
+  Future<int> getId() async {
+    try {
+      final prefs = await SharedPreferences.getInstance();
+      final id = prefs.getInt('id') ?? 0;
+      return id;
+    } catch (e) {
+      rethrow;
+    }
   }
 }
